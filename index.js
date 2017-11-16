aac28c6cd43da3e0dd7decb9bf9d289246ff7a1d
Christopher Carr <christopher.andrew.carr@gmail.com>
2017-11-16 12:11:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file.
+function lowerCaseDrivers (driversArray){
+  return driversArray.map(function (driver){
+    return driver.toLowerCase();
+  });
+}
+
+
+function nameToAttributes (driversArray) {
+  return driversArray.map(function (driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };
+  });
+}
+
+
+function attributesToPhrase (driversArray) {
+  return driversArray.map(function(driver){
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
